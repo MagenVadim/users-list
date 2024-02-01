e197e55e95a6f956677d09ffd4d5044d91ed1d4e
Vadim Smagin <magenvadim@gmail.com>
2024-02-01 02:45:49 +0200

    #1 first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Users from './Components/Users/Users'
+import { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users items={users} isLoading={isLoading}/>
     </div>
   );
 }
